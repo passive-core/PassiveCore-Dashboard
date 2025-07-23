9ca614ade46dc6bf2b1933d0edea8dff2d83e5b2
Travis Wilson <tjw102088@gmail.com>
2025-07-23 04:34:48 +0000

    Relocate dashboard code to repo root

---
+++ src/app/files/page.tsx
@@ -0,0 +1,8 @@
+export default function FilesPage() {
+  return (
+    <main className="p-8">
+      <h1 className="text-2xl font-bold mb-4">Files Explorer</h1>
+      {/* TODO: Fetch from /api/files and render a table of path, added_by, added_at */}
+    </main>
+  )
+}
